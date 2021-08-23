430266ee74562845424cc67e33f5ada694d8d06a
DoglasRocha <doglas_rocha@protonmail.ch>
2021-08-22 23:58:09 -0300

    end of the third class

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const start = () => {
+    $('.inicio').hide()
+
+    $('.fundo-game').append('<div id="jogador"></div>')
+    $('.fundo-game').append('<div id="inimigo1"></div>')
+    $('.fundo-game').append('<div id="inimigo2"></div>')
+    $('.fundo-game').append('<div id="amigo"></div>')
+}
