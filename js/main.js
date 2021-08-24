c0abefb5daa06599b72e617ade925ac98dc5edf6
DoglasRocha <doglas_rocha@protonmail.ch>
2021-08-23 23:26:23 -0300

    end of the fifth class

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ const start = () => {
     $('.beginning').hide(); // hides the "inicio" div
 
     // appends the "player", "enemy1", "enemy2" and "friend" divs in the "game-background" div
-    $('.game-background').append('<div id="player"></div>');
-    $('.game-background').append('<div id="enemy1"></div>');
-    $('.game-background').append('<div id="enemy2"></div>');
-    $('.game-background').append('<div id="friend"></div>');
+    $('.game-background').append('<div id="player" class="animation1";></div>');
+    $('.game-background').append('<div id="enemy1" class="animation2"></div>');
+    $('.game-background').append('<div id="enemy2" ></div>');
+    $('.game-background').append('<div id="friend" class="animation3"></div>');
 }
